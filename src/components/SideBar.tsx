03ccd60dc3725bb3d4361262d2df87590678ab8f
lichen404 <f360family@gmail.com>
2022-03-07 23:47:07 +0800

    SideBar 添加动画

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,85 +4,105 @@ import {NavLink} from "react-router-dom";
 import Icon from "./Icon";
 import {Context} from "../context";
 
-const SideBarWrapper = styled.ul`
+const SideBarWrapper = styled.aside`
+  
+  > ul {
+    background-color: #f0f0f0;
+    transition: all .3s;
+    position: absolute;
+    top: 0;
+    width: 200px;
+    min-height: 100vh;
+    left: -200px;
 
-  width: 200px;
-  background-color: #f0f0f0;
-  position: absolute;
-  top: 0;
-  left: 0;
-  min-height: 100vh;
-
-  > li {
-    line-height: 44px;
-    height: 44px;
-    cursor: pointer;
-
-
-    &:hover {
-
-      background-color: #b8bfc6;
+    &.open-sidebar {
+      left: 0;
     }
 
-    &.close-button-wrapper {
-      background: #f0f0f0;
-      display: flex;
-      align-items: center;
-      padding: 0 8px 0 32px;
-      justify-content: space-between;
+    > li {
+      line-height: 44px;
+      height: 44px;
+      cursor: pointer;
 
 
-      > h3 {
-        font-size: 18px;
-      }
+      &:hover {
 
-      > svg {
-        width: 1.5em;
-        height: 1.5em;
-        cursor: pointer;
-        -webkit-app-region: no-drag;
-      }
-
-    }
-
-    > a {
-      display: block;
-      width: 100%;
-      padding-left: 32px;
-      color: #000;
-
-      &.selected {
         background-color: #b8bfc6;
       }
 
+      &.close-button-wrapper {
+        background: #f0f0f0;
+        display: flex;
+        align-items: center;
+        padding: 0 8px 0 32px;
+        justify-content: space-between;
+
+
+        > h3 {
+          font-size: 18px;
+        }
+
+        > svg {
+          width: 1.5em;
+          height: 1.5em;
+          cursor: pointer;
+          -webkit-app-region: no-drag;
+        }
+
+      }
+
+      > a {
+        display: block;
+        width: 100%;
+        padding-left: 32px;
+        color: #000;
+
+        &.selected {
+          background-color: #b8bfc6;
+        }
+
+
+      }
+
 
     }
-
-
   }
+
+
+
+
 
 
 `
 const SideBar: FC = () => {
     const {isSidebarVisible, setIsSidebarVisible} = useContext(Context)
     return (
-        isSidebarVisible ? <SideBarWrapper>
-            <li className='close-button-wrapper'>
-                <h3>菜单</h3>
-                <Icon name="menu-off" onClick={() => {
-                    setIsSidebarVisible(false)
-                }}/>
-            </li>
-            <li>
-                <NavLink to="/upload" activeClassName="selected"> 上传</NavLink>
 
-            </li>
-            <li>
-                <NavLink to="/history" activeClassName="selected"> 历史记录</NavLink>
-            </li>
 
-        </SideBarWrapper> : null
+        <SideBarWrapper>
+            <ul className={isSidebarVisible && 'open-sidebar'}>
+                <li className='close-button-wrapper'>
+                    <h3>菜单</h3>
+                    <Icon name="menu-off" onClick={() => {
+
+                        setIsSidebarVisible(false)
+                    }}/>
+                </li>
+                <li>
+                    <NavLink to="/upload" activeClassName="selected"> 上传</NavLink>
+
+                </li>
+                <li>
+                    <NavLink to="/history" activeClassName="selected"> 历史记录</NavLink>
+                </li>
+            </ul>
+
+
+        </SideBarWrapper>
+
+
     )
 }
+
 
 export default SideBar;
