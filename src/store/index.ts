6d4797da0d6438f45dda78e9d61dc5139add6355
lichen404 <f360family@gmail.com>
2022-07-28 00:00:04 +0800

    使用indexedDB

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,30 @@
-import Store from 'electron-store';
+export const handleOpenDB = (databaseName:string, storeName:string, version = 1):Promise<any> =>{
+    return new Promise((resolve, reject) => {
+        let db;
+        const indexedDB = window.indexedDB
+        const request = indexedDB.open(databaseName, version);
+        request.onsuccess = ()=> {
+            db = request.result // 数据库对象
+            resolve(db);
+        };
 
+        request.onerror = (e)=> {
+           reject(e)
+        };
 
-const store = new Store();
+        request.onupgradeneeded = ()=> {
+            db = request.result
 
-export default  store;
+            if (!db.objectStoreNames.contains(storeName)) {
+                const objectStore = db.createObjectStore(storeName, { keyPath: "id" });
+                objectStore.createIndex("workerId", "workerId", { unique: false });
+                objectStore.createIndex("building", "building", { unique: false });
+                objectStore.createIndex("equipment", "equipment", { unique: false });
+                objectStore.createIndex("jobType", "jobType", { unique: false });
+                objectStore.createIndex("detail", "detail", { unique: false });
+                objectStore.createIndex("startDate", "startDate", { unique: false });
+                objectStore.createIndex("endDate", "endDate", { unique: false });
+            }
+        };
+    });
+}
