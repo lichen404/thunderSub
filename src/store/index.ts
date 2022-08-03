0cc4e5fb0d869e51b5bb308aa11dedfd3c31b559
lichen <f360family@gmail.com>
2022-08-03 09:03:05 +0800

    使用 indexedDB (WIP)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,30 +1,66 @@
-export const handleOpenDB = (databaseName:string, storeName:string, version = 1):Promise<any> =>{
+const maxDataLength = 30
+
+export const handleOpenDB = (databaseName: string, storeName: string, version = 1): Promise<any> => {
     return new Promise((resolve, reject) => {
         let db;
         const indexedDB = window.indexedDB
         const request = indexedDB.open(databaseName, version);
-        request.onsuccess = ()=> {
+        request.onsuccess = () => {
             db = request.result // 数据库对象
             resolve(db);
         };
 
-        request.onerror = (e)=> {
-           reject(e)
+        request.onerror = (e) => {
+            reject(e)
         };
 
-        request.onupgradeneeded = ()=> {
+        request.onupgradeneeded = () => {
             db = request.result
 
             if (!db.objectStoreNames.contains(storeName)) {
-                const objectStore = db.createObjectStore(storeName, { keyPath: "id" });
-                objectStore.createIndex("workerId", "workerId", { unique: false });
-                objectStore.createIndex("building", "building", { unique: false });
-                objectStore.createIndex("equipment", "equipment", { unique: false });
-                objectStore.createIndex("jobType", "jobType", { unique: false });
-                objectStore.createIndex("detail", "detail", { unique: false });
-                objectStore.createIndex("startDate", "startDate", { unique: false });
-                objectStore.createIndex("endDate", "endDate", { unique: false });
+                const objectStore = db.createObjectStore(storeName, {keyPath: "url"});
+                objectStore.createIndex("name", "name", {unique: false});
+                objectStore.createIndex("path", "path", {unique: false});
+                objectStore.createIndex("date", "date", {unique: false});
             }
         };
     });
 }
+
+export const readLatestData = (db: IDBDatabase, storeName: string): any[] => {
+    const list = []
+    const objectStore = db.transaction(storeName).objectStore(storeName)
+    const {result: cursor} = objectStore.openCursor()
+    let i = 0
+    if (cursor && i < maxDataLength) {
+        list.push(cursor.value)
+        i++
+    }
+    return list
+}
+
+export const updateData = (db: IDBDatabase, storeName: string, data: any):Promise<Event> => {
+    return new Promise((resolve, reject) => {
+        const request = db.transaction([storeName], "readwrite").objectStore(storeName).put(data)
+        request.onsuccess = (e) => {
+            resolve(e)
+        }
+        request.onerror = (e) => {
+            reject(e)
+        }
+    })
+}
+
+export const addData = (db: IDBDatabase, storeName: string, data: any): Promise<Event> => {
+    return new Promise((resolve, reject) => {
+        const request = db.transaction([storeName], "readwrite").objectStore(storeName).add(data)
+        request.onsuccess = (e) => {
+            resolve(e)
+        }
+        request.onerror = (e) => {
+            reject(e)
+        }
+    })
+}
+
+
